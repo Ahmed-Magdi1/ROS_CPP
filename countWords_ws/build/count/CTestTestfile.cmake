118ea0f5e52a82b0e53d5e0e8d002331e0516692
Ahmed Magdi <128742811+Ahmed-Magdi1@users.noreply.github.com>
2023-08-31 19:50:23 +0300

    Delete countWords_ws/build directory

--- countWords_ws/build/count/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahmed/countWords_ws/src/count
-# Build directory: /home/ahmed/countWords_ws/build/count
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- countWords_ws/build/count/catkin_generated/count-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(count_MESSAGE_FILES "")
-set(count_SERVICE_FILES "/home/ahmed/countWords_ws/src/count/srv/cont.srv")
